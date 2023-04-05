9ffc700727875313e064f271e30f07e4582b7a13
kty990 <masterhotdog9@gmail.com>
2023-04-05 16:27:54 -0600

    .

--- src/index.js
+++ src/index.js
@@ -8,8 +8,26 @@ const request = (url) => {
     });
 }
 
-const GetLinks = () => {
+const NextLink = (str, i) => {
+    let open = str.indexOf("<a", (i || 0));
+    let close = str.indexOf("</a>", (open || 0));
 
+    if (open != -1 && close != -1) {
+        let x_open = str.indexOf("href=\"", open);
+        let x_close = str.indexOf("\"", x_open);
+        return [str.substring(x_open, x_close), close];
+    }
+    return null;
+}
+
+const GetLinks = (ReqResult) => {
+    let links = [];
+    let link = NextLink(ReqResult, 0);
+    while (link !== null) {
+        links.push(link);
+        link = NextLink(ReqResult, link[1])
+    }
+    return links;
 }
 
 class ParentNode {
@@ -27,18 +45,12 @@ class ParentNode {
         }
         return this;
     }
-}
 
-class ChildNode {
-    /**
-     * 
-     * @param {*} parent : The parent of this node
-     *  
-     * This node type should only be used at the bottom of that branch in the tree
-     */
-    constructor(parent) {
-        this.parent = parent || null;
-        this.nodeType = nodeType;
+    AddChildren(parentNodeChain) {
+        if (this.GetParent() != this) {
+            this.GetParent().AddChildren(parentNodeChain);
+        }
+        // FINISH THIS
     }
 }
 
@@ -66,17 +78,21 @@ class Scraper {
      *         - Done by sending through run and incrementing
      */
     async run(url, iteration) {
-        let root = new ParentNode(null,null);
+        let root = new ParentNode(null, null);
         root.scope = (iteration || 1);
         let CurrentRequest = await request(url);
         root.data = CurrentRequest;
         let links = GetLinks(CurrentRequest);
         for (let x = 0; x < links.length; x++) {
             let z = await this.run(links[x].url, (iteration || 2));
-            let parent = z.GetParent();
-            // Get the current scope level
+            root.AddChildren(z);
             // If parent node, set in children of higher scope
         }
         return root
     }
 }
+
+/**
+ * ParentNode.GetLowestChild() <-- May not be required
+ * ParentNode.AddChlidren(<ParentNode>) <-- Takes a ParentNode (potentially a parent node list) and iterates through self. If iteration is same level, add as next parent node. If it is 1 less, add as child. 
+ */w
